02d711dda7ba062cb199c6534dc66a562827732c
a1824jj <a1824jj@aiit.ac.jp>
2020-03-25 11:06:02 +0900

    use bind two way

--- app/src/main/java/a1824jj/jp/ac/aiit/viewmodelsampel2_ktx/MainActivity.kt
+++ app/src/main/java/a1824jj/jp/ac/aiit/viewmodelsampel2_ktx/MainActivity.kt
@@ -4,14 +4,12 @@ import a1824jj.jp.ac.aiit.viewmodelsampel2_ktx.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
-
     private lateinit var viewModelFactory: MainActivityViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +17,8 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModelFactory = MainActivityViewModelFactory(123)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
-
-        viewModel.totalData.observe(this, Observer{
-            binding.resultTextView.text = it.toString()
-        })
-
-        binding.apply {
-            addButton.setOnClickListener{
-                viewModel.setTotal(inputEditText.text.toString().toInt())
-            }
-        }
+        binding.myViewModel = viewModel
+        binding.lifecycleOwner = this
 
     }
 }
--- app/src/main/java/a1824jj/jp/ac/aiit/viewmodelsampel2_ktx/MainActivityViewModel.kt
+++ app/src/main/java/a1824jj/jp/ac/aiit/viewmodelsampel2_ktx/MainActivityViewModel.kt
@@ -1,24 +1,38 @@
 package a1824jj.jp.ac.aiit.viewmodelsampel2_ktx
 
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel(startingTotal: Int) : ViewModel() {
+class MainActivityViewModel(startingTotal: Int) : ViewModel(), Observable {
 
     private var total = MutableLiveData<Int>()
-
     val totalData: LiveData<Int>
     get() = total
 
+    @Bindable
+    val inputText = MutableLiveData<String>()
+
     init {
         total.value = startingTotal
+        inputText.value = ""
     }
 
-    fun getTotal() = total.value
+    fun getTotal() = total.value!!.toString()
 
-    fun setTotal(input: Int) {
+    fun setTotal() {
+        val input = inputText.value!!.toInt()
         total.value = (total.value)?.plus(input)
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        //@Bindable build error fix
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        //@Bindable build error fix
     }
 
 }
